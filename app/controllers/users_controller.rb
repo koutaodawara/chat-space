585437ede90591da6ff32d08e74b4103ef265e48
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2017-02-09 15:27:15 +0900

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE ?', "%#{params[:name]}%")
+    @users_except_current_user = User.where('name LIKE(?)', "#{params[:query]}%").where.not(id: current_user.id)
+    users_except_current_user = users.where.not(name: current_user.name)
     respond_to do |format|
       format.html{ redirect_to users_path }
-      format.json{ render json: @users }
+      format.json{ render json: @users_expect_current_user }
     end
   end
 end
