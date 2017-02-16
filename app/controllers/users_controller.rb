0ab7bbd8dbe5c2ac7bab880b3374202f5514b953
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2017-02-16 17:01:36 +0900

    modify user files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
 
   def index
-    @users_except_current_user = User.where('name LIKE(?)', "#{params[:query]}%").where.not(id: current_user.id)
-    users_except_current_user = users.where.not(name: current_user.name)
+    @users_except_current_user = User.where('name LIKE(?)', "#{params[:input]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html{ redirect_to users_path }
-      format.json{ render json: @users_expect_current_user }
+      format.json{ render json: @users_except_current_user}
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def followed
+    @user = User.find(params[:id])
+    @users = @user.followed
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.follower
   end
 end
